694e3072a8c390564d0f87cf4a38d4b66eb7f2b0
TUANSPC\Dell Vostro 14 <19110494@student.hcmute.edu.vn>
2022-10-27 21:51:45 +0700

    build

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,13 +4,16 @@ const planetsRouter = require('./routes/planets/planets.router');
 const path = require('path');
 const app = express();
 
-const getPath = path.join(__dirname, '../public');
-app.use(express.static(getPath));
-console.log(getPath)
+
 app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
