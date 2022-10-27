371de9aff0ddfa230341834131ff1876105a4729
TUANSPC\Dell Vostro 14 <19110494@student.hcmute.edu.vn>
2022-10-27 17:05:14 +0700

    th commit

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
+const path = require('path');
 const app = express();
 
+const getPath = path.join(__dirname, '../public');
+app.use(express.static(getPath));
+console.log(getPath)
 app.use(cors({
     origin: "http://localhost:3000"
 }));
